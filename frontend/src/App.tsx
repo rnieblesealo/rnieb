e59e7d681af2b46f3f04fb043d22dbfd80839481
Rafael Niebles <rnieblesealo@gmail.com>
2026-03-18 12:19:33 -0400

    improved frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,37 @@ interface Drawing {
   path: string
 }
 
+const DrawingTile = ({ data }: { data: Drawing }) => {
+  const [hovered, setHovered] = useState(false)
+
+  return (
+    // entire tile 
+    <div
+      className="w-48 flex flex-col items-center justify-center"
+      onMouseEnter={() => { setHovered(true) }}
+      onMouseLeave={() => { setHovered(false) }}
+    >
+      <div className="relative">
+        {/* image itself */}
+        <img
+          src={`http://localhost:8080/${data.path}`}
+          className="w-fit aspect-square object-cover"
+        />
+        {/* description overlay */}
+        {hovered &&
+          <div
+            className="absolute top-0 opacity-80 w-full h-full text-white text-sm bg-black flex flex-col items-center justify-center">
+            <span className="italic font-bold mt-4 mb-[-1] text-md">"{data.name}"</span>
+            <span className="opacity-75 m-4 text-center text-sm overflow-auto">
+              {data.description}
+            </span>
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
+
 const Collage = () => {
   const [drawings, setDrawings] = useState<Drawing[]>([])
 
@@ -24,16 +55,9 @@ const Collage = () => {
 
   return (
     /* render the images */
-    <div className="grid grid-cols-3 w-128">
+    <div className="grid grid-cols-2 md:grid-cols-3 w-fit">
       {drawings && drawings.map(drawing => (
-        <div key={drawing.id}>
-          <span>{drawing.name}</span>
-          <img
-            src={`http://localhost:8080/${drawing.path}`}
-            className="w-xs aspect-1/1 object-cover"
-          />
-          <span>{drawing.description}</span>
-        </div>
+        <DrawingTile key={drawing.id} data={drawing} />
       ))}
     </div>
   )
@@ -59,31 +83,36 @@ export default function App() {
       </div >
 
       {/* upload image form */}
-      <form
-        action="http://localhost:8080/upload"
-        method="POST"
-        encType="multipart/form-data"
-        className="flex flex-col items-start gap-3 m-4"
-      >
-        <input
-          type="text"
-          name="name"
-          placeholder="Name..."
-        />
-        <input
-          type="text"
-          name="description"
-          placeholder="Description..."
-        />
-        <input
-          id="upload-images"
-          type="file"
-          name="file"
-          accept="image/*"
-          className="w-fit"
-        />
-        <button type="submit" className="bg-red-500 text-black p-4 w-fit">Upload</button>
-      </form>
+      <div className="flex flex-col items-center justify-center m-6 text-red-500">
+        <span className="font-bold mb-6">Upload a Drawing</span>
+        <form
+          action="http://localhost:8080/upload"
+          method="POST"
+          encType="multipart/form-data"
+          className="flex flex-col items-start gap-3 m-4"
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name..."
+            className="w-full"
+          />
+          <textarea
+            name="description"
+            placeholder="Description..."
+            rows={4}
+            className="w-full"
+          />
+          <input
+            id="upload-images"
+            type="file"
+            name="file"
+            accept="image/*"
+            className="w-full"
+          />
+          <button type="submit" className="bg-red-500 text-black p-2 w-full font-bold ">Upload</button>
+        </form>
+      </div>
     </div>
   )
 }
