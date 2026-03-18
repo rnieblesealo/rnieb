2d07f69e4c21ac0b3d5f5a7435de7a452ab34e6e
Rafael Niebles <rnieblesealo@gmail.com>
2026-03-18 09:39:08 -0400

    polish frontend a bit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,31 +18,54 @@ const Collage = () => {
 
   return (
     /* render the images */
-    <div>
+    <div className="grid grid-cols-3 w-128">
       {imageFilenames && imageFilenames.map(imageFilename => (
-        <img key={imageFilename} src={`http://localhost:8080/uploads/${imageFilename}`} />
+        <img
+          key={imageFilename}
+          src={`http://localhost:8080/uploads/${imageFilename}`}
+          className="w-xs aspect-1/1 object-cover"
+        />
       ))}
     </div>
   )
 }
 
 export default function App() {
+  const [logo, setLogo] = useState('')
+  const [deco, setDeco] = useState('')
+
+  axios.get("/logo.txt").then(res => { setLogo(res.data) })
+  axios.get("/stars.txt").then(res => { setDeco(res.data) })
+
   return (
-    <>
+    <div className="w-full h-min flex flex-col items-center justify-center">
+      <div className="flex flex-row gap-5 text-xs m-6">
+        <pre>{deco}</pre>
+        <pre>{logo}</pre>
+      </div>
+
+      {/* display images */}
+      <div>
+        <Collage />
+      </div >
+
       {/* upload image form */}
       <form
         action="http://localhost:8080/upload"
         method="POST"
         encType="multipart/form-data"
+        className="flex flex-col items-center gap-3 m-4"
       >
-        <input type="file" name="image" /> {/* puts this imagae under the "image" field; look at curl */}
-        <button type="submit">Upload</button>
+        <label htmlFor="upload-images" className="font-bold w-fit">Upload Images</label>
+        <input
+          id="upload-images"
+          type="file"
+          accept="image/*"
+          multiple
+          className="w-fit"
+        />
+        <button type="submit" className="bg-red-500 text-black p-4 w-fit">Upload</button>
       </form>
-
-      {/* display images */}
-      <div>
-        <Collage />
-      </div>
-    </>
+    </div>
   )
 }
